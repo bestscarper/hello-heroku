3e6fae7913c729947236e2effa1e41f54ec3aab1
Ashley Hindmarsh <ash+github@best-scarper.co.uk>
2018-09-23 22:18:06 +0100

    Add db config

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const http = require('http');
+const { Client } = require('pg');
 
 const port = process.env.PORT || 5000;
+const { DATABASE_URL } = process.env
 
 const server = http.createServer((req, res) => {
+  const client = new Client({
+      connectionString: DATABASE_URL
+  });
   res.statusCode = 200;
   res.setHeader('Content-type', 'text/plain');
-  res.end('Hello, universe\n');
+  client.connect()
+    .then( () => client.query('SELECT * from hellotable'))
+    .then( (result) => {
+      res.end(`${result.rows[0].name}\n`);
+      client.end();
+    })
+    .catch(() => {
+      res.end('ERROR');
+      client.end();
+    });
 });
 
 server.listen(port, () => {
